1d4d1d1a290a078796a0510211c63364fb6c8d66
Eloilson <eloilson.ageedu@gmail.com>
2023-11-15 23:41:49 -0300

    feat: map page implementation

---
+++ src/components/Mapbox.vue
@@ -0,0 +1,101 @@
+<template>
+  <div ref="map" class="map-container"></div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl';
+
+
+export default {
+  name: 'Mapbox',
+  data() {
+    return {
+      map: null,
+      directions: null,
+      marker: null,
+      token: 'pk.eyJ1IjoiZWxvaWxzb25kb3NhbmpvcyIsImEiOiJja2g5b29xZDgwdXp1MnBrMW1ubjFxY25uIn0.MV6dnRrRJ7rbSxNPfWLFvQ'
+    };
+  },
+  async mounted() {
+    try {
+      mapboxgl.accessToken = this.token
+
+      this.map = new mapboxgl.Map({
+        container: this.$refs.map,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-37.978384, -4.935428],
+        zoom: 15,
+      });
+
+      this.marker = new mapboxgl.Marker({
+        color: '#FFD666'
+      }).setLngLat([-37.978384, -4.935428]).addTo(this.map);
+
+      const apiKey = this.token
+      const url = `https://api.mapbox.com/directions/v5/mapbox/driving/-37.978384%2C-4.935428%3B-37.9860609%2C-4.93601?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token=${apiKey}`;
+
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        this.directions = data;
+      } catch (error) {
+        console.error('Erro ao obter direções:', error);
+      }
+
+      this.map.addLayer({
+        id: 'route',
+        name:'Eloilson dos Anjos',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: this.directions.routes[0].geometry.coordinates,
+            },
+          },
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round',
+        },
+        paint: {
+          'line-color': '#888',
+          'line-width': 8,
+        },
+      })
+
+      if (this.marker) {
+        this.marker.remove();
+      }    
+      
+      const markers = this.directions.routes[0].geometry.coordinates
+
+      if (markers.length >= 2) {
+      const firstMarker = markers[0];
+      const lastMarker = markers[markers.length - 1];
+
+      this.marker = new mapboxgl.Marker({ color: '#FFD666' })
+        .setLngLat(firstMarker)
+        .addTo(this.map);
+
+      new mapboxgl.Marker()
+      .setLngLat(lastMarker)
+      .addTo(this.map);
+    }
+
+    } catch (error) {
+    }    
+  },
+}
+</script>
+
+<style scoped>
+.map-container {
+  width: 100vw;
+  height: 100vh;
+  color: black;
+}
+</style>
---
+++ src/views/MapView.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="page-map">
+    <aside>
+      <header>
+        <img src="../assets/map-marker.svg" alt="logo happy" class="map-marker" />
+        <h2>Escolha um orfanato no mapa</h2>
+        <p>Muitas crianças estãoesperando a sua visita :)</p>
+      </header>
+
+      <footer>
+        <strong>Russas</strong>
+        <span>Ceará</span>
+      </footer>
+    </aside>
+    <Mapbox />
+    <router-link to="/orphanage" class="create-orphanage">
+      <fa icon="plus" class="icon" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+import Mapbox from '../components/Mapbox.vue';
+
+export default {
+  components: {
+    Mapbox,
+  },
+};
+</script>
+
+<style scoped>
+#page-map {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  position: relative;
+}
+
+#page-map aside {
+  width: 440px;
+  background: var(--gradiente-blue, linear-gradient(330deg, #29B6D1 0%, #00C7C7 100%));
+  padding: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+#page-map aside h2 {
+  font-size: 40px;
+  font-weight: 800;
+  line-height: 42px;
+  margin-top: 64px;
+}
+
+#page-map aside p {
+  line-height: 28px;
+  margin-top: 24px;
+}
+
+#page-map aside footer {
+  display: flex;
+  flex-direction: column;
+  line-height: 24px;
+}
+
+#page-map aside footer strong {
+  font-weight: 800;
+}
+
+#page-map .create-orphanage {
+  position: absolute;
+  right: 40px;
+  bottom: 40px;
+
+  width: 64px;
+  height: 64px;
+  background: #15C3D6;
+  border-radius: 20px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.2s;
+  color: #FFF;
+}
+
+#page-map .create-orphanage:hover {
+  background: #17D6EB;
+}
+
+</style>
